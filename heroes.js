96e86e2998727debca2ad1b3c19abdf0d0350ef8
nicole-cs <53199020+nicole-cs@users.noreply.github.com>
2019-07-25 15:14:54 -0500

    Create heroes.js

---
+++ heroes.js
@@ -0,0 +1,7 @@
+var heroes=[
+    new Heroe(4,"Bolbo","Bolson"),
+    new Heroe(5,"Frido","Bolson"),
+    new Heroe(6,"Gandal","El mago Gris"),
+    new Heroe(7,"Chapulin","Colorado"),
+    new Heroe(8,"Capitan","Escudo")
+];
